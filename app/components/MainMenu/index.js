dc30a667db21859475cbd2181b3ec7ec4750614f
Eliot GODARD <eliot.godard@gmail.com>
2017-08-07 15:59:24 +0200

    tweaked main menu + css

--- app/components/MainMenu/index.js
+++ app/components/MainMenu/index.js
@@ -4,7 +4,10 @@ import {html} from 'snabbdom-jsx';
 
 export function MainMenu(sources) {
 
-	const {DOM, save} = sources;
+	const {DOM} = sources;
+	const save = sources.save ? sources.save : {};
+
+	console.log(save);
 
 	const action$ = xs.merge(
 		DOM.select('.js-new-game').events('click').mapTo({type: "newGame"}),
@@ -24,7 +27,7 @@ export function MainMenu(sources) {
 		<div className="menu-principal">
 			<h1>Menu Principal</h1>
 			<button className="js-new-game button-3d">NEW GAME</button>
-			<button className="js-resume-game button-3d">RESUME GAME</button>
+			{save.round + 1 ? <button className="js-resume-game button-3d">RESUME GAME<br/>Round {save.round + 1}</button> : ""}
 		</div>
 	);
 
